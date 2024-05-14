72c80917b0e217ba71bd67d87710cad40e93545b
satrio adi <143088360+kale16@users.noreply.github.com>
2024-05-14 15:11:42 +0700

    fix

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./slices/counterSlice";
+import userReducer from "./slices/userSlice";
+const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+    user: userReducer,
+  },
+});
+export default store;
